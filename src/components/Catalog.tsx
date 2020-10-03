7cb87210e4014ce77292dbf88ca42d74aa726a9e
sirmcclay <mcclay.design@gmail.com>
2020-10-03 16:34:41 -0300

    Create cart reducer, rootReducer, end right architecture for store

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 import { useSelector } from 'react-redux';
 
 export const Catalog: React.FC = () => {
-  const email = useSelector(state => state.email);
+  const catalog = useSelector(state => state);
 
-  console.log(email)
+  console.log(catalog)
 
   return (
     <h1>Catalog</h1>
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,6 @@
 import { createStore } from 'redux';
+import rootReducer from './modules/rootReducer';
 
-const store = createStore(() => {
-  return {
-    id: 3,
-    name: 'Clayton',
-    email: 'clayton@eu.com.br'
-  }
-});
+const store = createStore(rootReducer);
 
 export default store;
