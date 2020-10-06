3ba58b918a9821741711292ffc504cd7e4a3a1a7
sirmcclay <mcclay.design@gmail.com>
2020-10-06 12:39:10 -0300

    Change state interface to store ids from products out of stock and show this info on CatalogItem component

--- src/components/CatalogItem.tsx
+++ src/components/CatalogItem.tsx
@@ -1,5 +1,6 @@
 import React, { useCallback } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { IState } from '../store';
 
 import { addProductToCartRequest } from '../store/modules/cart/actions';
 
@@ -11,6 +12,10 @@ interface CatalogItemProps {
 
 const CatalogItem: React.FC<CatalogItemProps> = ({ product }) => {
   const dispatch = useDispatch();
+
+  const hasFailedStockCheck = useSelector<IState, boolean>(state => {
+    return state.cart.failedStockCheck.includes(product.id);
+  })
 
   const handleAddProductToCart = useCallback(() => {
     dispatch(addProductToCartRequest(product));
@@ -27,6 +32,8 @@ const CatalogItem: React.FC<CatalogItemProps> = ({ product }) => {
       >
         Comprar
       </button>
+
+  { hasFailedStockCheck && <span style={{ color: 'red' }}>Falta de estoque</span> }
     </article>
   );
 }
